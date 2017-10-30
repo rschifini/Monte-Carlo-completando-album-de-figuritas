703ebbccfd88a1dc86886c20b4d68d628b0f0528
rschifini <rschifini@gmail.com>
2017-10-30 13:23:40 -0300

    Imputacion de figuritas
    Se corrigio la imputacion de las figuritas repetidas (antes no se calculaba bien).

--- Simulacion.R
+++ Simulacion.R
@@ -1,22 +1,28 @@
 figuritas = 200
-amigos = 70
+SobresXAmigo = c()
+Grupo =  c(1:10,seq(20,100,5))
 
-resultados = c()
-for(k in 1:10000){
-  albumLLeno = FALSE
-  sobres = 0
-  album = rep(0,200)
-  while (!albumLLeno){
-    # Se abren tantos sobres como amigos
-    paquete = floor(runif(amigos * 5,1,figuritas+1))
-    sobres = sobres + amigos
-    # Agregar las cartas al pool
-    album[paquete] = album[paquete] + 1
-    # Si todas estan repetida al menos la cantidad de amigos entonces finalizar 
-    if(all(album >= amigos)){
-      albumLLeno = T
+for(amigos in Grupo){
+  
+  resultados = c()
+  for(k in 1:1000){
+    albumLLeno = FALSE
+    sobres = 0
+    album = rep(0,200)
+    while (!albumLLeno){
+      # Se abren tantos sobres como amigos
+      paquete = floor(runif(amigos * 5,1,figuritas+1))
+      sobres = sobres + amigos
+      # Agregar las cartas al pool
+      for(j in paquete){
+        album[j] = album[j] + 1  
+      }
+      # Si todas estan repetida al menos la cantidad de amigos entonces finalizar 
+      if(all(album >= amigos)){
+        albumLLeno = T
+      }
     }
+    resultados = c(resultados,sobres)
   }
-  resultados = c(resultados,sobres)
+  SobresXAmigo = c(SobresXAmigo, mean(resultados)/amigos)
 }
-print(mean(resultados)/amigos)
