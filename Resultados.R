ebf62a7812ebdbcf567984b6399da05e9cecf9a0
rschifini <rschifini@gmail.com>
2017-10-30 12:29:57 -0300

    Create Resultados.R

---
+++ Resultados.R
@@ -0,0 +1,6 @@
+SobresXAmigo = c(235, 162, 134, 119, 109, 102, 89, 80, 79, 82, 86, 91, 98)
+Grupo = c(1:6, 10, 20, 30, 40, 50, 60, 70)
+plot(Grupo, SobresXAmigo, 
+     main = "Sobres por amigo para completar 1 album cada",
+     xlab = "Amigos",
+     ylab = "Sobres Promedio")
